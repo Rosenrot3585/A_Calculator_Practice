bfbe35444358398dff2478184c7bfadcb9f39bb3
Rosenrot3585 <mcmp3585@gmail.com>
2023-12-16 00:09:56 -0300

    sum and substract methods created

--- acalculator/src/Calculator.java
+++ acalculator/src/Calculator.java
@@ -1,4 +1,11 @@
 public class Calculator {
     
-    }
+    public void sumar(int a, int b){
+        System.out.println("La suma es: " + (a+b));
+    };
+
+    public void restar(int a, int b){
+        System.out.println("La resta es: " + (a-b));
+    };
+}
 
