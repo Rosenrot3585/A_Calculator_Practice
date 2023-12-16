6909a1ca78407120b7e21668836a9898a7bac569
Rosenrot3585 <mcmp3585@gmail.com>
2023-12-16 00:11:41 -0300

    multiplication and division methods created

--- acalculator/src/Calculator.java
+++ acalculator/src/Calculator.java
@@ -1,4 +1,12 @@
 public class Calculator {
     
-    }
+    public void multiply(int a, int b) {
+        System.out.println(a * b);
+    };
+
+    public void divide(int a, int b) {
+        System.out.println(a / b);
+    };
+
+}
 
